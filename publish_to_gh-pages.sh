6a97a50687b5fd7f281879acb5f0f447408481ee
Mallika Duggal <mallikajeevun@gmail.com>
2020-12-21 16:55:33 -0500

    add google analytics

--- publish_to_gh-pages.sh
+++ publish_to_gh-pages.sh
@@ -77,7 +77,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+HUGO_ENV=production hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publish to gh-pages (publish.sh)" && git push
